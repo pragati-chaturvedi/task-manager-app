3687f74489b5bfc204b007afd7975671ec8b7d1c
Pragati Chaturvedi <pragatichaturvedi@Pragatis-MacBook-Pro-3.local>
2024-11-07 16:47:52 -0800

    Updates

---
+++ precache-manifest.25950c6575dbee13c66099aff6b3bf92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24b0fb68abfb7cec749464071a6128c8",
+    "url": "/task-manager-app/index.html"
+  },
+  {
+    "revision": "b43f232e0b60d689ec84",
+    "url": "/task-manager-app/static/css/2.876f1645.chunk.css"
+  },
+  {
+    "revision": "b43f232e0b60d689ec84",
+    "url": "/task-manager-app/static/js/2.d4ae6b03.chunk.js"
+  },
+  {
+    "revision": "e26de4f0e0d98f1626b38316e0457409",
+    "url": "/task-manager-app/static/js/2.d4ae6b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bc3e58d680d6d753616",
+    "url": "/task-manager-app/static/js/main.78c449ba.chunk.js"
+  },
+  {
+    "revision": "e545b52acaa85bc0263a",
+    "url": "/task-manager-app/static/js/runtime-main.132ba3b7.js"
+  }
+]);
